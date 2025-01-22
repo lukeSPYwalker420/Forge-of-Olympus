7001ee7138699bf5a866188c110561eaf587b6a9
lukeSPYwalker420 <kieren2203@googlemail.com>
2025-01-22 21:10:10 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(express.static(path.join(__dirname)));
 // Define routes or additional middleware here
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
