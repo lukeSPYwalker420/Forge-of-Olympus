e1b5fb724dfccf99b9d9858081ca845ec0956479
lukeSPYwalker420 <kieren2203@googlemail.com>
2026-04-04 22:53:30 +0100

    Delete test-load.js

--- test-load.js
+++
@@ -1,8 +0,0 @@
-import fs from "fs";
-
-const program = JSON.parse(
-  fs.readFileSync("./data/powerlifting-program.json", "utf-8")
-);
-
-console.log("Weeks loaded:", [...new Set(program.map(x => x.week))]);
-console.log("Total sessions:", program.length);
