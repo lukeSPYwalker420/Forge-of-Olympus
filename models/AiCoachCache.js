836d8fcec88e26484839a5c99e9ace7404aa279a
lukeSPYwalker420 <kieren2203@googlemail.com>
2026-04-27 16:23:42 +0100

    Add files via upload

--- models/AiCoachCache.js
+++ models/AiCoachCache.js
@@ -2,12 +2,8 @@ import mongoose from "mongoose";
 
 const AiCoachCacheSchema = new mongoose.Schema({
   userId: { type: String, required: true, unique: true },
-  prompts: [{
-    lift: String,
-    type: String,
-    message: String
-  }],
-  createdAt: { type: Date, default: Date.now, expires: '24h' }   // auto-delete after 24h
+  prompts: [{ type: mongoose.Schema.Types.Mixed }],   // ← array of objects
+  createdAt: { type: Date, default: Date.now, expires: '24h' }
 });
 
 export default mongoose.model("AiCoachCache", AiCoachCacheSchema);
